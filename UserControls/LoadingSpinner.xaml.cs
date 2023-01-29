416ee69a4e6821c06d262f82933d76cf1052d440
indeedornot <gravitybartek@gmail.com>
2023-01-29 16:09:22 +0100

    feat: Add spinner when loading store

---
+++ UserControls/LoadingSpinner.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace WPFTodo.UserControls;
+/// <summary>
+/// Interaction logic for LoadingSpinner.xaml
+/// </summary>
+public partial class LoadingSpinner : UserControl {
+    public LoadingSpinner() {
+        InitializeComponent();
+    }
+}
