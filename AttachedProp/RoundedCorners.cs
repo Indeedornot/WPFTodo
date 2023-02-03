66ed23c5e1a1822e645ed86169fb6cacf572cad6
indeedornot <gravitybartek@gmail.com>
2023-02-03 14:44:06 +0100

    ref: Styles
    fix: TextboxWaterkmarkTemplate respect padding
    ref: Use Template for Textboxwatermark rather than style to not block style slot
    
    ref: Change RoundedCorners to static values rather than style to not block style slot
    feat: Use attachedProp for rounded corners

---
+++ AttachedProp/RoundedCorners.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFTodo.AttachedProp;
+/// <summary>
+/// This class is used to add a corner radius to a control.
+/// <br/> Requires control to have a border as a child, otherwise it will not work.
+/// <br/> Does not support visibility changes!
+/// </summary>
+public static class RoundedCorners {
+    #region Radius
+    public static readonly DependencyProperty RadiusProperty = DependencyProperty.RegisterAttached(
+        "Radius",
+        typeof(CornerRadius),
+        typeof(RoundedCorners),
+        new PropertyMetadata(default(CornerRadius), RadiusChanged));
+
+    public static void SetRadius(DependencyObject element, CornerRadius value) {
+        element.SetValue(RadiusProperty, value);
+    }
+
+    public static CornerRadius GetRadius(DependencyObject element) {
+        return (CornerRadius)element.GetValue(RadiusProperty);
+    }
+
+    public static void RadiusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        Border? border = d as Border;
+        if (border == null) {
+            border = UIHelper.FindChild<Border>(d);
+        }
+
+        if (border == null) {
+            InitialRadiusSet(d, e);
+            return;
+        }
+
+        border.CornerRadius = (CornerRadius)e.NewValue;
+    }
+
+    private static void InitialRadiusSet(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var element = d as Control;
+        if (element == null) return;
+
+        element.Loaded -= LoadedHandler;
+        element.IsVisibleChanged -= IsVisibleChangedHandler;
+
+        if (!element.IsLoaded) {
+            element.Loaded += LoadedHandler;
+        }
+        else {
+            element.IsVisibleChanged += IsVisibleChangedHandler;
+        }
+    }
+
+    private static void LoadedHandler(object sender, RoutedEventArgs e) {
+        var element = sender as Control;
+        if (element == null) return;
+
+        //Pass it off to visibility handler
+        if (!element.IsVisible) {
+            element.IsVisibleChanged -= IsVisibleChangedHandler;
+            element.IsVisibleChanged += IsVisibleChangedHandler;
+            return;
+        }
+
+        //Ensure children are loaded
+        element.UpdateLayout();
+
+        var border = UIHelper.FindChild<Border>(element);
+        if (border == null) return;
+
+        var radius = GetRadius(element);
+        border.CornerRadius = radius;
+    }
+    private static void IsVisibleChangedHandler(object sender, DependencyPropertyChangedEventArgs e) {
+        var element = sender as Control;
+        if (element == null) return;
+
+        //Ensure children are loaded
+        element.UpdateLayout();
+
+        var border = UIHelper.FindChild<Border>(element);
+        if (border == null) return;
+
+        var radius = GetRadius(element);
+        border.CornerRadius = radius;
+    }
+
+    public static readonly DependencyProperty TopLeftProperty = DependencyProperty.RegisterAttached(
+      "TopLeft",
+      typeof(double),
+      typeof(RoundedCorners),
+      new PropertyMetadata(default(double), TopLeftChanged));
+
+    public static void SetTopLeft(DependencyObject element, double value) {
+        element.SetValue(TopLeftProperty, value);
+    }
+
+    public static double GetTopLeft(DependencyObject element) {
+        return (double)element.GetValue(TopLeftProperty);
+    }
+
+    private static void TopLeftChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var cornerRadius = GetRadius(d);
+        cornerRadius.TopLeft = (double)e.NewValue;
+        SetRadius(d, cornerRadius);
+    }
+    #endregion
+
+    #region TopRight
+    public static readonly DependencyProperty TopRightProperty = DependencyProperty.RegisterAttached(
+        "TopRight",
+        typeof(double),
+        typeof(RoundedCorners),
+        new PropertyMetadata(default(double), TopRightChanged));
+
+    public static void SetTopRight(DependencyObject element, double value) {
+        element.SetValue(TopRightProperty, value);
+    }
+
+    public static double GetTopRight(DependencyObject element) {
+        return (double)element.GetValue(TopRightProperty);
+    }
+
+    private static void TopRightChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var cornerRadius = GetRadius(d);
+        cornerRadius.TopRight = (double)e.NewValue;
+        SetRadius(d, cornerRadius);
+    }
+    #endregion
+
+    #region BottomLeft
+    public static readonly DependencyProperty BottomLeftProperty = DependencyProperty.RegisterAttached(
+        "BottomLeft",
+        typeof(double),
+        typeof(RoundedCorners),
+        new PropertyMetadata(default(double), BottomLeftChanged));
+
+    public static void SetBottomLeft(DependencyObject element, double value) {
+        element.SetValue(BottomLeftProperty, value);
+    }
+
+    public static double GetBottomLeft(DependencyObject element) {
+        return (double)element.GetValue(BottomLeftProperty);
+    }
+
+    private static void BottomLeftChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var cornerRadius = GetRadius(d);
+        cornerRadius.BottomLeft = (double)e.NewValue;
+        SetRadius(d, cornerRadius);
+    }
+    #endregion
+
+    #region BottomRight
+    public static readonly DependencyProperty BottomRightProperty = DependencyProperty.RegisterAttached(
+        "BottomRight",
+        typeof(double),
+        typeof(RoundedCorners),
+        new PropertyMetadata(default(double), BottomRightChanged));
+
+    public static void SetBottomRight(DependencyObject element, double value) {
+        element.SetValue(BottomRightProperty, value);
+    }
+
+    public static double GetBottomRight(DependencyObject element) {
+        return (double)element.GetValue(BottomRightProperty);
+    }
+
+    private static void BottomRightChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var cornerRadius = GetRadius(d);
+        cornerRadius.BottomRight = (double)e.NewValue;
+        SetRadius(d, cornerRadius);
+    }
+    #endregion
+
+    #region Left
+    public static readonly DependencyProperty LeftProperty = DependencyProperty.RegisterAttached(
+        "Left",
+        typeof(double),
+        typeof(RoundedCorners),
+        new PropertyMetadata(default(double), LeftChanged));
+
+    public static void SetLeft(DependencyObject element, double value) {
+        element.SetValue(LeftProperty, value);
+    }
+
+    public static double GetLeft(DependencyObject element) {
+        return (double)element.GetValue(LeftProperty);
+    }
+
+    private static void LeftChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var cornerRadius = GetRadius(d);
+        cornerRadius.TopLeft = (double)e.NewValue;
+        cornerRadius.BottomLeft = (double)e.NewValue;
+        SetRadius(d, cornerRadius);
+    }
+    #endregion
+
+    #region Right
+    public static readonly DependencyProperty RightProperty = DependencyProperty.RegisterAttached(
+        "Right",
+        typeof(double),
+        typeof(RoundedCorners),
+        new PropertyMetadata(default(double), RightChanged));
+
+    public static void SetRight(DependencyObject element, double value) {
+        element.SetValue(RightProperty, value);
+    }
+
+    public static double GetRight(DependencyObject element) {
+        return (double)element.GetValue(RightProperty);
+    }
+
+    private static void RightChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var cornerRadius = GetRadius(d);
+        cornerRadius.TopRight = (double)e.NewValue;
+        cornerRadius.BottomRight = (double)e.NewValue;
+        SetRadius(d, cornerRadius);
+    }
+    #endregion
+
+    #region Top
+    public static readonly DependencyProperty TopProperty = DependencyProperty.RegisterAttached(
+        "Top",
+        typeof(double),
+        typeof(RoundedCorners),
+        new PropertyMetadata(default(double), TopChanged));
+
+    public static void SetTop(DependencyObject element, double value) {
+        element.SetValue(TopProperty, value);
+    }
+
+    public static double GetTop(DependencyObject element) {
+        return (double)element.GetValue(TopProperty);
+    }
+
+    private static void TopChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var cornerRadius = GetRadius(d);
+        cornerRadius.TopLeft = (double)e.NewValue;
+        cornerRadius.TopRight = (double)e.NewValue;
+        SetRadius(d, cornerRadius);
+    }
+    #endregion
+
+    #region Bottom
+    public static readonly DependencyProperty BottomProperty = DependencyProperty.RegisterAttached(
+        "Bottom",
+        typeof(double),
+        typeof(RoundedCorners),
+        new PropertyMetadata(default(double), BottomChanged));
+
+    public static void SetBottom(DependencyObject element, double value) {
+
+        element.SetValue(BottomProperty, value);
+    }
+
+    public static double GetBottom(UIElement element) {
+        return (double)element.GetValue(BottomProperty);
+    }
+
+    private static void BottomChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var cornerRadius = GetRadius(d);
+        cornerRadius.BottomLeft = (double)e.NewValue;
+        cornerRadius.BottomRight = (double)e.NewValue;
+        SetRadius(d, cornerRadius);
+    }
+    #endregion
+}
---
+++ AttachedProp/UIHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WPFTodo.AttachedProp;
+
+internal static class UIHelper {
+    /// <summary>
+    /// Finds a Child of a given item in the visual tree. 
+    /// </summary>
+    /// <param name="parent">A direct parent of the queried item.</param>
+    /// <typeparam name="T">The type of the queried item.</typeparam>
+    /// <param name="childName">x:Name or Name of child. </param>
+    /// <returns>The first parent item that matches the submitted type parameter. 
+    /// If not matching item can be found, 
+    /// a null parent is being returned.</returns>
+    public static T? FindChild<T>(DependencyObject parent)
+       where T : DependencyObject {
+        // Confirm parent and childName are valid. 
+        if (parent == null) return null;
+
+        T? foundChild = null;
+
+        int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+        for (int i = 0; i < childrenCount; i++) {
+            var child = VisualTreeHelper.GetChild(parent, i);
+
+            // If the child is not of the request child type child
+            T? childType = child as T;
+            if (childType != null) {
+                // child element found.
+                foundChild = (T)child;
+                break;
+            }
+
+            // recursively drill down the tree
+            foundChild = FindChild<T>(child);
+
+            // If the child is found, break so we do not overwrite the found child. 
+            if (foundChild != null) break;
+
+        }
+
+        return foundChild;
+    }
+
+    internal static T? FindParent<T>(DependencyObject element) where T : FrameworkElement {
+        FrameworkElement? parent = VisualTreeHelper.GetParent(element) as FrameworkElement;
+        if (parent == null) return null;
+
+        if (parent is T correctlyTyped) {
+            return correctlyTyped;
+        }
+
+        return FindParent<T>(parent);
+    }
+}
